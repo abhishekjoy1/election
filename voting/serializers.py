99c18ecc230e025e8375c825e3efe7b3c51cf0e5
abhishek <abhishek.mallik@myntra.com>
2016-03-05 21:00:46 +0530

    added state and booth wise user creation and ajax and django rest web service

--- voting/serializers.py
+++ voting/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import routers, serializers, viewsets
-from voting.models import CustomUser
+from voting.models import CustomUser, State, Booth
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'voter_id')
+
+class StateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = State
+        fields = ('id', 'name')
+
+class BoothSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = State
+        fields = ('id', 'name')
