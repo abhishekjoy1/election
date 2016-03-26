6b4ee2fa10b1447af71cc4a040da7cc079ccefb9
abhishek <abhishek.mallik@myntra.com>
2016-03-26 09:22:08 +0530

    disabled adding user through django admin

--- voting/admin.py
+++ voting/admin.py
@@ -7,6 +7,9 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('first_name','last_name', 'voter_id', 'casted_vote', 'is_staff')
     search_fields = ['voter_id']
 
+    def has_add_permission(self, request):
+        return False
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Party)
